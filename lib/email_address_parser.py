eba63291c93c9711c9d149d6590af04771003436
Sadiq <sadiq.said@student.moringaschool.com>
2023-11-07 12:21:31 +0300

    Completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, input_string):
+        self.input_string = input_string
+
+    def parse(self):
+        # Use a regular expression to find email addresses in the input string
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}\b'
+        email_addresses = re.findall(email_pattern, self.input_string)
+
+        return email_addresses
+
+
+
